8496dc4994e7c993a57b00d3e9409694acb6dcc8
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-30 17:32:05 +0100

    feat: table trains

--- database/migrations/2024_01_30_155444_create_trains_table.php
+++ database/migrations/2024_01_30_155444_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            $table->string('company', 30);
+            $table->string('departure_station', 20);
+            $table->string('arrival_station', 20);
+            $table->time('departure_time', 4);
+            $table->time('arrival_time', 4);
+            $table->string('train_id', 50);
+            $table->tinyInteger('number_of_carriages')->unsigned()->nullable();
+            $table->boolean('on_time', 50);
+            $table->boolean('cancelled', 50);
+
             $table->timestamps();
         });
     }
