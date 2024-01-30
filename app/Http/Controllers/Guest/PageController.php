5c40d24b332bf9a6d9723b937563bab3f7078bc8
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-30 18:32:39 +0100

    feat: stampa in pagina dei dati

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -12,8 +13,6 @@ class PageController extends Controller
     {
 
         $trains = Train::all();
-
-        dd($trains);
 
         return view('welcome', compact('trains'));
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
+<?php
+use Carbon\Carbon;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,11 +13,38 @@
     <!-- Includiamo gli assets con la direttiva vite -->
     @vite('resources/js/app.js')
 </head>
-<body class="p-5 bg-dark text-light  d-flex justify-content-center flex-wrap">
-    <h1 class="w-100 text-center p-4">Laravel Template</h1>
-    <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo">
-   <div class="w-100 text-center p-4">
-     <h3>Bootstrap</h3>
-    <h3>Scss</h3></div>
+
+<body class="p-5 bg-dark text-light d-flex justify-content-center flex-wrap flex-column ">
+    <h1 class="mb-5">Trains</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Train ID</th>
+                <th scope="col">Departure Station</th>
+                <th scope="col">Departure Date</th>
+                <th scope="col">Departure Time</th>
+                <th scope="col">Arrival Station</th>
+                <th scope="col">Arrival Date</th>
+                <th scope="col">Arrival Time</th>
+                <th scope="col">Company</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td scope="row">{{ $train->train_id }}</td>
+                    <td scope="row">{{ $train->departure_station }}</td>
+                    <td scope="row">{{ $train->departure_date }}</td>
+                    <td scope="row">{{ \Carbon\Carbon::parse($train['departure_time'])->format('H:i') }}</td>
+                    <td scope="row">{{ $train->arrival_station }}</td>
+                    <td scope="row">{{ $train->arrival_date }}</td>
+                    <td scope="row">{{ \Carbon\Carbon::parse($train['arrival_time'])->format('H:i') }}</td>
+                    <td scope="row">{{ $train->company }}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 </body>
+
 </html>
