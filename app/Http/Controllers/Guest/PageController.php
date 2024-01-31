9326d60959ce4d9b71f38177fd61dde938ffdf78
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-31 11:33:14 +0100

    correzione

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
-use Carbon\Carbon;
+// use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $currentDate = Carbon::now()->format('Y-m-d');
+        $currentDate = now()->format('Y-m-d');
 
         $trains = Train::all()->where('departure_date', $currentDate);
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <?php
-use Carbon\Carbon;
-
+// use Carbon\Carbon;
 ?>
 <!DOCTYPE html>
 <html lang="en">
